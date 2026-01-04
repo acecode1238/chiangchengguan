6392f1693db9337a82e5d920509c1b3d8473678e
chiang cheng guan <156899651+acecode1238@users.noreply.github.com>
2026-01-04 22:40:09 +0800

    update button

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-  function startVideo() {
-    const video = document.querySelector('.bg-video');
-    if (video) video.play().catch(() => {});
-  }
-  
-  window.addEventListener('scroll', startVideo, { once: true });
-  window.addEventListener('touchstart', startVideo, { once: true });
-});
-
-
 const text = document.querySelector('.typewriter')
 const content = text.textContent;
 
@@ -25,3 +14,12 @@ function type() {
 }
 
 type()
+
+const response = document.querySelector('.send');
+
+response.addEventListener('click', function(){
+     window.open(
+        "https://mail.google.com/mail/?view=cm&fs=1&to=chiangchengguan@gmail.com",
+        "_blank"
+    );
+})
